714d626fbbd4680f31d891fc86b9bbe6de0331cf
ShubhamSharma43 <oo7shubham4347@gmail.com>
2021-05-04 18:14:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
-}
+  Empname='';
+  Empnumber:any;
+  name=[];
+  details={}
+  Sno=1;
+  
+  submit(){
+   
+    this.details={
+      sno:this.Sno,
+      name:this.Empname,
+      number:this.Empnumber,
+      }
+      
+    this.name.push(this.details);
+   this.Sno++;
+  }
+
+  delete(){
+    
+  }
+
+  }
+  
+  
+
