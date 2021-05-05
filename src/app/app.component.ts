e4d51ed48bac1ceadb21fd60a8f5ea502e0a9ece
ShubhamSharma43 <oo7shubham4347@gmail.com>
2021-05-05 19:01:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,28 +9,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'todo';
   Empname='';
-  Empnumber:any;
-  name=[];
+  Empnumber='';
+  data=[];
   details={}
+ index=0;
   Sno=1;
-  
   submit(){
-   
-    this.details={
+   this.details={
       sno:this.Sno,
       name:this.Empname,
       number:this.Empnumber,
-      }
-      
-    this.name.push(this.details);
+      index:this.index
+}
+   this.data.push(this.details);
    this.Sno++;
-  }
-
-  delete(){
-    
-  }
-
-  }
+   this.index++;
+}
+  delete(index){
+   this.data.splice(index,1);
+   for(let i=index;i<this.data.length;i++){
+     this.data[i].index--;
+     this.data[i].sno--;
+   }
+}
+}
+  
   
   
 
