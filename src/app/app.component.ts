8783d22a6b3bc9e51995eef6c3c497b19213a30f
ShubhamEng99 <70835695+ShubhamEng99@users.noreply.github.com>
2021-05-09 22:45:05 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,10 @@ import {LoginModule} from './login/login.module';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-<<<<<<< HEAD
- 
-}
-=======
-  title='angular poc';
-  public Empname:String;
-  public Empnumber:Number;
-  Empdata=[];
-  Empdetails={};
-  dataindex=0;
-  Sno=1;
-  submit(){
-   this.Empdetails={sno:this.Sno,name:this.Empname,number:this.Empnumber,index:this.dataindex};
-   this.Empdata.push(this.Empdetails);
-   this.Sno++;
-   this.dataindex++;
-  }
-  delete(delindex){
-   this.Empdata.splice(delindex,1);
-   for(let i=delindex;i<this.Empdata.length;i++){
-     this.Empdata[i].index--;
-     this.Empdata[i].sno--;
-   }
-  }
- }
+export class AppComponent {}
+
   
   
   
 
->>>>>>> 310f0eeda443852d6744f4a85c196af3979caf37
+
